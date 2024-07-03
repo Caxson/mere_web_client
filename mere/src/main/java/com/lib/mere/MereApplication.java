6d4fc897e115d6094b98e99a59a51b36a3285de3
caosen <13123728009@163.com>
2024-07-03 21:09:03 +0800

    init: 项目初始化

---
+++ mere/src/main/java/com/lib/mere/MereApplication.java
@@ -0,0 +1,13 @@
+package com.lib.mere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MereApplication.class, args);
+    }
+
+}
---
+++ mere/src/test/java/com/lib/mere/MereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib.mere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
